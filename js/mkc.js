e5efad146392a11d5a9575761de5a3e2ae9ec515
Michael K. Clements <michaelkc@me.com>
2015-07-01 22:59:34 +0100

    More updates to work section

--- js/mkc.js
+++ js/mkc.js
@@ -8,19 +8,30 @@ function ready(fn) {
     }
 }
 ready(function () {
-    
+
     function ViewModel() {
         var self = this;
-        
+
         self.isNavClicked = ko.observable(false);
-        
-        self.openNav = function () {
-            self.isNavClicked(!self.isNavClicked())
+        self.openNav = function() {
+          self.isNavClicked(!self.isNavClicked())
         };
 
+
+        self.isFocusActive = ko.observable(false);
+        self.focusItem = function() {
+          self.isFocusActive(!self.isFocusActive())
+        };
+
+        workItems: ko.observableArray([
+          { title: 'MKC Music', url: 'http://michaelkc.com/music'},
+          { title: 'Old MichaelKC.com', url: 'http://michaelkc.com/old'},
+          { title: 'Lit Fuse Films', url: 'http://litfusefilms.com' }
+        ])
+
     };
-    
+
     // Activates knockout.js
     ko.applyBindings(new ViewModel());
-    
+
 });
